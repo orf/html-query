f6aa5353c79a8dc6250f7599fa8bba803cd1e9d6
Tom Forbes <tom@tomforb.es>
2023-04-14 16:51:01 +0200

    Formatting

--- html-query-ast/src/lib.rs
+++ html-query-ast/src/lib.rs
@@ -1,10 +1,10 @@
 mod parser;
 
-use std::collections::HashMap;
+pub use crate::parser::{Action, Expression};
 use nom::error::VerboseError;
 use nom::Finish;
-pub use crate::parser::{Action, Expression};
 pub use parser::format_error;
+use std::collections::HashMap;
 
 pub fn parse_string(input: &str) -> Result<HashMap<&str, Action>, VerboseError<&str>> {
     let (_, hashmap) = crate::parser::object(input).finish()?;
